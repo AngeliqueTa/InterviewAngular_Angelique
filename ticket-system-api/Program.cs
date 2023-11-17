ec06e8de6e0013bb935db2bd39b7a67a8591704c
007000793 <blushy777@gmail.com>
2023-11-17 10:59:00 +0200

    feat: Remove createHostBuilder

--- ticket-system-api/Program.cs
+++ ticket-system-api/Program.cs
@@ -14,7 +14,7 @@ namespace ticket_system_api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
